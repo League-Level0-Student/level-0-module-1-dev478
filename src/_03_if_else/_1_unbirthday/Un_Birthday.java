5d151ed30a41f9a6355ec9aa242a542006cf78a5
root <root@1adcd5528e49>
2020-07-30 23:58:41 +0000

    Unbirtday

---
+++ src/_03_if_else/_1_unbirthday/Un_Birthday.java
@@ -0,0 +1,11 @@
+package _03_if_else._1_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class Un_Birthday {
+public static void main(String[] args) {
+
+	
+	JOptionPane.showInputDialog(null,"What is your birthday?");
+}
+}
