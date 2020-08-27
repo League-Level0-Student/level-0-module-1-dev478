ad02984f95e7edae3b46e48884211f4b187d54e0
root <root@45206734cf83>
2020-08-27 23:57:44 +0000

    Commit!??!??! robot in space

--- src/_04_int/_2_robot_in_space/RobotInSpace.java
+++ src/_04_int/_2_robot_in_space/RobotInSpace.java
@@ -28,12 +28,21 @@ public class RobotInSpace implements KeyEventDispatcher {
 	private void moveRobot(int keyPressed) throws InterruptedException {
 		// 2. Print out the keyPressed variable and write down the numbers for
 		// each arrow key
-
+System.out.println(keyPressed);
 		// 3. If the up arrow is pressed, move the Robot up the screen.
-
+if(keyPressed == 38) {
+rob.setAngle(0);
+rob.microMove(3);
+}
 		// 4. If the down arrow is pressed, move the Robot down.
-
+System.out.println(keyPressed);
+if(keyPressed == 40) {
+	rob.setAngle(180);
+	rob.microMove(3);
+}
 		// 5. If the left arrow is pressed, make the Robot go left.
+System.out.println(keyPressed);
+	
 
 		// 6. If right is pressed, move the Robot right.
 
